8b4b2a2886ec6d66dfe23453d89485fc6a332ef9
Nate Gadgibalaev <nat@xnsv.ru>
2012-10-25 18:05:52 +0400

    no line numbers in coderay

--- lib/zenpush/markdown.rb
+++ lib/zenpush/markdown.rb
@@ -4,7 +4,7 @@ require 'kramdown'
 module ZenPush
   class Markdown
     def self.to_zendesk_html(file)
-      Kramdown::Document.new(File.read(file)).to_html
+      Kramdown::Document.new(File.read(file), coderay_line_numbers: false).to_html
     end
   end
 end
